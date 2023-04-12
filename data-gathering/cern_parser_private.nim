c78dd8731b76784abcf79a8655dccb3f7f57dbc1
C <randazzocary@gmail.com>
2023-04-12 18:02:15 -0500

    private crawler update

--- data-gathering/cern_parser_private.nim
+++ data-gathering/cern_parser_private.nim
@@ -26,3 +26,39 @@ if login(session, url, email, password):
   echo document.body
 else:
   quit(1)
+
+
+  ####################
+  #[
+
+import httpclient, strutils
+
+# Define the login credentials
+const
+  username = "your_username"
+  password = "your_password"
+
+# Create a `HttpClient` instance
+var client = newHttpClient()
+
+# Prepare the login request
+let
+  url = "https://example.com/login"
+  headers = {"Content-Type": "application/x-www-form-urlencoded"}
+  body = "username=" & urlEncode(username) & "&password=" & urlEncode(password)
+
+# Send the login request
+let response = client.post(url, headers, body)
+
+# Check the response status code
+if response.statusCode == 200:
+  # Login successful, perform scraping or other actions
+  echo "Login successful!"
+  # ...
+else:
+  # Login failed, handle error
+  echo "Login failed with status code: ", response.statusCode
+  echo "Response body: ", response.body
+
+
+  ]#
